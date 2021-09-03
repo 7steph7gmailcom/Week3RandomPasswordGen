0d6d5a258efa18482d756448e1704b6ea82f59e6
stephaniecjohansson <7steph7@gmail.com>
2021-09-02 20:55:07 -0600

    selection

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Assign Characters
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lower = "abcdefghijklmnopqrstuvwxyz";
@@ -22,12 +21,45 @@ function enterpassword(){
 
 //generatePassword function gets called in writePassword function, returning the final password
 function generatePasswords(){
-
+ //do random generation here and return the final password in the end
+ var result = "";
+ //ask the user how many characters they want
+ var length = prompt("How many characters in your random password? (between 8 and 128)");
+ if(isNaN(length)){
+   alert("You must input a number!");
+   return generatePassword()
+ }
+ if(length<8 || length> 128){
+   alert("Please choose numbers between 8 - 128!");
+   return generatePassword()
+ }
 }
 
 
+//User needs to confirm if they want to include lower, upper, num, special?
+var hasUpper = confirm("Include upper case letters?");
+var hasLower = confirm("Include lower case letters?");
+var hasNumbers = confirm("Include numbers?");
+var hasSpecial = confirm("Include special characters?");
 
 
 
+if(){
+  
+}
+if(hasUpper){
+  chosenCharacters += lower
+}
+if(hasLower){
+  chosenCharacters += upper
+}
+if(hasNumbers) {
+  chosenCharacters += numbers
+}
+if(hasSpecial) {
+  chosenCharacters += special
+}
 
+// (for loop)
 
+return
