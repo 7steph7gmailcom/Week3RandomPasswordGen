91f7143649473d9ce14b04bda0a0011ea1cd5b3d
stephaniecjohansson <7steph7@gmail.com>
2021-09-03 21:04:47 -0600

    saved

--- script.js
+++ script.js
@@ -1,45 +1,51 @@
 // Assign Characters
+
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var special = "!@,#$%&*{}[]/\+=";
 var chosenCharacters = "";
 
+var generateBtn = document.querySelector("#generate");
+
 // Add event listener generates button
 generateBtn.addEventListener("click", writePassword)
 
-var generateBtn = document.querySelector("#generate");
-
-// Write Password to the #password input
-function enterpassword(){
+//#password input
+function enterPassword(){
   var password = genratepassword;
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-
+//write password function 
+function writePassword(){
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
 //generatePassword function calls writePassword function => returning the final password
-function generatePasswords(){
+function generatePassword(){
+  console.log(generatePassword)
  var result = "";
   //Prompt user how many characters in Password
  var length = prompt("How many characters would you like in your Password? (between 8 and 128)");
  if(isNaN(length)){
-   alert("Number must be greater than 8");
+   alert("A number between 8 -128 must be selected to continue")
    return generatePassword()
  }
- if(length<8 || length> 128){
+ if(length < 8 || length > 128){
    alert("Number must be less than 128");
    return generatePassword()
  }
 
-
-
 //User needs to confirm to include lower, upper, num, special
-var hasLower = confirm("Include lower case letters?");
-var hasUpper = confirm("Include upper case letters?");
-var hasNumbers = confirm("Include numbers?");
-var hasSpecial = confirm("Include special characters?");
+ var hasLower = confirm("Include lower case letters?");
+ var hasUpper = confirm("Include upper case letters?");
+ var hasNumbers = confirm("Include numbers?");
+ var hasSpecial = confirm("Include special characters?");
+
 }
 
 
@@ -62,5 +68,5 @@ if(hasSpecial) {
 // (for loop)
 for (var i = 0; i < length; i++)
 
-return;
+// return;
 }
